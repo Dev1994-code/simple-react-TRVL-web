7f9bd4d0ab1f45fafe76c63ff92fcf7ae7793c6c
Dawit-Engdawork <dawitengdawork33@gmail.com>
2024-04-11 07:41:51 +0300

    TRVL

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+import { HeroSection } from '../HeroSection'
+import { Card } from '../Card'
+import { Footer } from '../Footer'
+
+export const Home = () => {
+  return <>
+    <HeroSection />
+    <Card />
+    <Footer />
+  </>
+}
